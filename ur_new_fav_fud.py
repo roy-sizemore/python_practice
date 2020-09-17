fb209e3798d8e851b3dfe460658ce2dba97ceb5d
roy-sizemore <67349682+roy-sizemore@users.noreply.github.com>
2020-09-16 20:30:31 -0400

    Your New Favourite Food

---
+++ ur_new_fav_fud.py
@@ -0,0 +1,9 @@
+# Your New Favourite Food
+# Demonstrates string concatenation
+
+print("I can tell you what your new favourite food will be. /nWhat are your two favourite foods?")
+fav_fud0 = input("First: ")
+fav_fud1 = input("Second: ")
+
+print("\nYour new favourite food is ", fav_fud0 + fav_fud1)
+
