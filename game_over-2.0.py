6673fae27e9a1512528164a6a7f2ece5976181bb
roy-sizemore <67349682+roy-sizemore@users.noreply.github.com>
2020-09-16 20:25:26 -0400

    Game Over 2.0

---
+++ game_over-2.0.py
@@ -0,0 +1,37 @@
+# Game Over
+# Demonstrates the use of quotes in strings
+
+print("Program 'Game Over' 2.0")
+
+print("Same", "message", "as before")
+
+print("Just",
+      "a bit"
+      "bigger")
+
+print("Here", end="")
+print("it is...")
+
+print(
+       r"""
+         _____       ___       ___  ___   _____
+        /  ___|     /   |     /   |/   | |  ___|
+        | |        / /| |    / /|   /| | | |__
+        | |  _    / ___ |   / / |__/ | | |  __|
+        | |_| |  / /  | |  / /       | | | |___
+        \_____/ /_/   |_| /_/        |_| |_____|
+
+         _____   _     _   _____   _____
+        /  _  \ | |   / / |  ___| |  _  \
+        | | | | | |  / /  | |__   | |_| |
+        | | | | | | / /   |  __|  |  _  /
+        | |_| | | |/ /    | |___  | | \ \
+        \_____/ |___/     |_____| |_|  \_\
+
+        """
+     )
+
+print("\n\nPress Enter to exit.")
+
+
+
