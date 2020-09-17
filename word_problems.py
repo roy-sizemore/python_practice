fe95328f660d122e98a9b5b2089fbcdd89e37514
roy-sizemore <67349682+roy-sizemore@users.noreply.github.com>
2020-09-16 20:30:17 -0400

    Word Problems

---
+++ word_problems.py
@@ -0,0 +1,7 @@
+# Word Problems
+# Demonstrates numbers and math
+
+print("If a 2000 pound pregnant hippo gives birth to a 100 pound calf,")
+print("but then eats 50 pounds of food, how much does she weigh?")
+print("2000 - 100 + 50 =", 2000 - 100 + 50)
+
